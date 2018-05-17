203f809c8613d3400561c2dedd77a165aa3c97be
ronald vilorio <soccer23415@gmail.com>
2018-05-17 17:06:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ const driversByName = function(drivers){
 const totalRevenue = function(drivers){
   // debugger
    return drivers.reduce(function(accumulator,currentValue){
-    return accumulator["revenue"] + currentValue["revenue"]},0)
+     accumulator["revenue"] + currentValue["revenue"]},0)
   // drivers.reduce(function(accumulator,currentValue){ return accumulator + currentValue["revenue"]})
 }
